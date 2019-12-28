7dd02d3bb31b9237b6d3eb183219989e75f2fc08
Tomasz Smereczynski <tomek.smereczynski@gmail.com>
2019-12-28 20:01:54 +0100

    css's

--- main/webapp/js/app.js
+++ main/webapp/js/app.js
@@ -1,14 +1,14 @@
 $(function () {
-    $("button").on("click",function(){
+    //$("button").on("click",function(){
         $.ajax({
-            url:"http://localhost:8282/books/"
+            url:"https://swapi.co/api/films",
         }).done(function(response){
             console.log(response.results)
             renderMovies(response.results)
         }).fail(function(err){
             console.log(err)
         })
-    })
+    //})
 
     function renderMovies(movies){
         movies.forEach(function(movie){
